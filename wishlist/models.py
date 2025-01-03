520e60d1981503cebb03f706462a9a96ae812ace
Gugonunes <gugonunes@hotmail.com>
2025-01-03 14:52:28 -0300

    feat(wishlist): add model for wishlist and migration

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from users.models import User
+from books.models import Book
 # Create your models here.
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    books = models.ManyToManyField(Book, related_name='wishlist')
+
+    def __str__(self):
+        return f"Lista de desejos de {self.user.username}"
