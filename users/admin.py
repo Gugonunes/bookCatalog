a9b033b1b7ca3d9898bd787d98abea05f9d0666c
Gugonunes <gugonunes@hotmail.com>
2025-01-03 14:54:22 -0300

    feat(users): Add configuration for users in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from users.models import Role, User
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username']
+
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Role, RoleAdmin)
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users import views as users_views
 
-app_name = 'home'
+app_name = 'home_users'
 
 urlpatterns = [
     path('', users_views.user_page, name='user_page')
