946ebdf7e7ce1934dfc378a0313d027faa67153f
Gugonunes <gugonunes@hotmail.com>
2025-01-03 14:54:32 -0300

    feat(wishlist): Add configuration for wishlist in the admin panel

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from wishlist.models import Wishlist
 
 # Register your models here.
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['list_books']
+
+    def list_books(self, obj):
+        books = obj.books.order_by('title')
+        return ', '.join([book.title for book in books])
+
+
+admin.site.register(Wishlist, WishlistAdmin)
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from wishlist import views as wishlist_views
 
-app_name = 'home'
+app_name = 'home_wishlist'
 
 urlpatterns = [
     path('', wishlist_views.wishlist_page, name='wishlist_page')
