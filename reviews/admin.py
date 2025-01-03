3bb2dc2599b3cddf61fd6114090eec04b2aac168
Gugonunes <gugonunes@hotmail.com>
2025-01-03 14:54:11 -0300

    feat(reviews): Add configuration for reviews in the admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from reviews.models import Review
 
 # Register your models here.
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ['created_at']
+
+
+admin.site.register(Review, ReviewsAdmin)
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from reviews import views as reviews_views
 
-app_name = 'home'
+app_name = 'home_reviews'
 
 urlpatterns = [
     path('', reviews_views.reviews_home, name='reviews_home'),
