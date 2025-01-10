264127635804c29ba419d3b8a6ce3e4f22d8fc27
Gugonunes <gugonunes@hotmail.com>
2025-01-09 21:19:48 -0300

    feat(books): fixing books view to use book id instead of index

--- books/views.py
+++ books/views.py
@@ -8,28 +8,8 @@ from django.conf.urls.static import static
 
 def books_home(request):
     all_books = Book.objects.all()
-    books_list = []
-    index = 0
-    for book in all_books:
-        index += 1
-        title = book.title
-        author = book.author
-        img_url = book.cover_image.url
-        publication_date = book.publication_date
-        categories = ", ".join(category.name for category in book.categories.all())
-        synopsis = book.synopsis
-
-        books_list.append({
-            'index': index,
-            'title': title,
-            'author': author,
-            'img_url': img_url,
-            'publication_date': publication_date,
-            'categories': categories,
-            'synopsis': synopsis
-        })
 
     content = {
-        'books': books_list
+        'books': all_books
     }
     return render(request, 'books/index.html', context=content)
