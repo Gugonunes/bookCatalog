b88072d3bb9bc160f981557f7c6bcb0256b3d83d
Gugonunes <gugonunes@hotmail.com>
2025-01-14 17:11:13 -0300

    debugger true

--- bookCatalog/settings.py
+++ bookCatalog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kvfv2*-b2p0jm#!)ab1uvzkk(a+wsm7f303f0l9-k%^af%d%=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
