086512d52f4fd0f6f9d2728ec1db40219b241a07
Gugonunes <gugonunes@hotmail.com>
2025-01-14 17:04:09 -0300

    first push to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn bookCatalog.wsgi:application --log-file -
--- bookCatalog/settings.py
+++ bookCatalog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-kvfv2*-b2p0jm#!)ab1uvzkk(a+wsm7f303f0l9-k%^af%d%=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +130,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+django_on_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
